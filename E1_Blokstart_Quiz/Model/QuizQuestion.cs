d727a49b545a531a962f8de03c983fa060011c05
MartijnCurio <124584804+MartijnCurio@users.noreply.github.com>
2024-08-26 09:52:49 +0200

    Migrated VS project to repository

---
+++ E1_Blokstart_Quiz/Model/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1_Blokstart_Quiz.Model
+{
+    internal class QuizQuestion
+    {
+        public string Question { get; set; }
+        public Dictionary<string, bool> Answers { get; set; }
+    }
+}
---
+++ E1_Blokstart_Quiz/Program.cs
@@ -0,0 +1,17 @@
+﻿using E1_Blokstart_Quiz.Model;
+
+List<object> questions = new()
+{
+    new QuizQuestion()
+    {
+        Question = "What is the capital of the Netherlands?",
+        Answers = new Dictionary<string, bool>()
+        {
+            { "Amsterdam", true },
+            { "Rotterdam", false },
+            { "The Hague", false },
+            { "Utrecht", false }
+        }
+    }
+};
+
